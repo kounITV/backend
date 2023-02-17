8072fa41a421fbcdbe02aed6b9f4f0b998508c78
kounITV <kounnapha7700@gmail.com>
2023-02-17 10:06:02 +0700

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+var bodyParser = require('body-parser')
+const cors = require('cors');
+
+const PORT = 3000;
+
+
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+  res.send("Hello React");
+});
+
+app.post("/login", (req, res) => {
+  if(req.body.username === 'koun' && req.body.password === '123456'){
+    res.send('Successful!!!');
+  }else{
+    res.send('Faild!!!');
+  }
+  
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
